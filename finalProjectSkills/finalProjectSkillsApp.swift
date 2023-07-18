5f578c45bb4bb07ce011638f0e7579f3578e738c
Kkiana22 <kianaylee@gmail.com>
2023-07-18 10:14:25 -0700

    Initial Commit

---
+++ finalProjectSkills/finalProjectSkillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectSkillsApp.swift
+//  finalProjectSkills
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectSkillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
